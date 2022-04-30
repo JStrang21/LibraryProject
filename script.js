e2b858ee6b494b7a9f13d292df131b8bbe019ed9
Justin Strang <Justinstrang100@gmail.com>
2022-04-30 17:13:43 -0400

    Added event listener for submit which uses Book constructor to make a new book with user input by using form input id's as arguments

--- script.js
+++ script.js
@@ -11,5 +11,11 @@ function Book(author, title, pages, read) {
 }
 
 submitButton.addEventListener('click', () => {
-    
+    const book = new Book(
+        document.getElementById('title'),
+        document.getElementById('author'),
+        document.getElementById('pages'),
+        document.getElementById('read'),
+    )
+    console.log(book)
 })
