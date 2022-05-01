23933182eb958aec815a8ff46657fba12852ef02
Justin Strang <Justinstrang100@gmail.com>
2022-05-01 14:09:14 -0400

    Added clear function to clear input from form so user can add additional books

--- script.js
+++ script.js
@@ -24,22 +24,40 @@ submitButton.addEventListener('click', () => {
     addBookToGrid(newBook);
 })
 
+//Takes book object created from Book constructor as argument to add book to grid 
 function addBookToGrid(newBook) {
     const bookGridContainer = document.querySelector('.bookContainer');
     let bookElement = document.createElement('div');
     bookElement.classList.add('bookElements');
+    //Adds element to grid which will contain one instance of book object 
     bookGridContainer.appendChild(bookElement);
+    //Passes newly created grid element and book object to function to create and store sub-elements of input
     addBookInfoToGridElement(bookElement, newBook);
+    //Resets form input after adding a book
+    clearForm();
 }
 
 function addBookInfoToGridElement(bookElement, newBook) {
+    //Sub-elements to display book info
     let titleElement = document.createElement('p');
     let authorElement = document.createElement('p');
     let pagesElement = document.createElement('p');
     let readElement = document.createElement('p');
+    
     bookElement.appendChild(titleElement);
     bookElement.appendChild(authorElement);
     bookElement.appendChild(pagesElement);
     bookElement.appendChild(readElement);
     
+    titleElement.innerHTML = newBook.title
+    authorElement.innerHTML = newBook.author
+    pagesElement.innerHTML = newBook.pages
+    readElement.innerHTML = newBook.read
+}
+
+function clearForm() {
+    document.getElementById('title').value = ""
+    document.getElementById('author').value = ""
+    document.getElementById('pages').value = ""
+    document.getElementById('read').checked = ""
 }
